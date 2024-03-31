d1e1ca31dbeb28037dff17f022a78e1b71c22e98
sanket <sanket.chaudhary@coditas.com>
2024-03-31 23:43:24 +0530

    Day-4

--- Day-4/main.go
+++ Day-4/main.go
@@ -1,10 +1,34 @@
 package main
 
-// import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func main(){
+func main() {
+	wg := &sync.WaitGroup{}
+
+	
 	element := make(chan int)
-	list:=[]int{2,4,6,8,10}
-	// fmt.Println(squareWorker(list,element))
-	squareWorker(list,element)
+	list := []int{2, 4, 6, 8, 10}
+
+	wg.Add(2)
+
+	go func(ch chan int, list []int, wg *sync.WaitGroup) {
+		defer wg.Done()
+		for _, v := range list {
+			ch <- v * v
+		}
+		close(ch) 
+	}(element, list, wg)
+
+	go func(ch chan int, wg *sync.WaitGroup) {
+		defer wg.Done()
+		sum :=0
+		for val := range ch {
+			sum+=val
+		}
+		fmt.Println("The Sum of list is :",sum)
+	}(element, wg)
+	wg.Wait()
 }

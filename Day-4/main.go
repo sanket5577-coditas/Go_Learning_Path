b04e9dbe138a7567f3c89d720bf1ddd3e15ebda7
sanket <sanket.chaudhary@coditas.com>
2024-04-01 00:20:16 +0530

    Improve variable and mutex for clarity

--- Day-4/main.go
+++ Day-4/main.go
@@ -6,29 +6,35 @@ import (
 )
 
 func main() {
-	wg := &sync.WaitGroup{}
+	var wg sync.WaitGroup
 
-	
-	element := make(chan int)
-	list := []int{2, 4, 6, 8, 10}
+	var mut sync.Mutex
+
+	squaredChan := make(chan int)
+	numbers := []int{2, 4, 6, 8, 10}
+	var sum int
 
 	wg.Add(2)
 
-	go func(ch chan int, list []int, wg *sync.WaitGroup) {
+	go func(ch chan int, nums []int, wg *sync.WaitGroup) {
 		defer wg.Done()
-		for _, v := range list {
-			ch <- v * v
+		for _, num := range nums {
+			ch <- num * num
 		}
-		close(ch) 
-	}(element, list, wg)
+		close(ch)
+	}(squaredChan, numbers, &wg)
 
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch chan int, wg *sync.WaitGroup, mut *sync.Mutex) {
 		defer wg.Done()
-		sum :=0
-		for val := range ch {
-			sum+=val
+		// sum := 0
+		for squaredNum := range ch {
+			fmt.Println("Squared element:", squaredNum)
+			mut.Lock()
+			sum += squaredNum
+			mut.Unlock()
 		}
-		fmt.Println("The Sum of list is :",sum)
-	}(element, wg)
+		fmt.Println("Sum of squared numbers:", sum)
+	}(squaredChan, &wg, &mut)
+
 	wg.Wait()
 }
